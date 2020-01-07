9c5322f4648ce550ba1e00253b55beb286e70ef9
brantmorton <brantmort@gmail.com>
2020-01-06 20:05:34 -0600

    fetching data from firebase backend

--- src/containers/CoffeePosts/CoffeePosts.js
+++ src/containers/CoffeePosts/CoffeePosts.js
@@ -1,39 +1,41 @@
 import React, { Component } from "react";
 
 import CoffeePost from "../../components/CoffeePost/CoffeePost";
-
+import axios from "axios";
 
 class CoffeePosts extends Component {
   state = {
-    roaster: null,
-    origin: null,
-    region: null,
-    process: null,
-    notes: null,
-    rating: null
+    posts: null
   };
 
+  componentDidMount() {
+    axios
+      .get("https://coffee-locker.firebaseio.com/posts.json")
+      .then(response => {
+        this.setState({ posts: response.data });
+      });
+  }
+
   render() {
-    return (
-      <div>
-        <CoffeePost
-          roaster="Onyx"
-          origin="Ethiopian"
-          region="Guji"
-          process="Natural"
-          notes={["strawberry", "floral", "stonefruit"]}
-          rating="8"
-        />
-        <CoffeePost
-          roaster="Madcap"
-          origin="Kenyan"
-          region="Gatchatha"
-          process="Washed"
-          notes={["tomato", "caramel", "chocolate"]}
-          rating="5"
-        />
-      </div>
-    );
+    let coffeePosts = <p>Loading...</p>;
+    if (this.state.posts) {
+      console.log(this.state.posts);
+      coffeePosts = Object.keys(this.state.posts).map(post => {
+        return (
+          <CoffeePost
+            roaster={this.state.posts[post].roaster}
+            origin={this.state.posts[post].origin}
+            region={this.state.posts[post].region}
+            process={this.state.posts[post].process}
+            notes={this.state.posts[post].notes}
+            rating={this.state.posts[post].rating}
+            key={post}
+          />
+        );
+      });
+    }
+
+    return <div>{coffeePosts}</div>;
   }
 }
 
