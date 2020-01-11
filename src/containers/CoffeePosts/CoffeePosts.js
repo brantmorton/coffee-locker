4de2e5db25481077d2f90eff831daa40e0bc13f6
brantmorton <brantmort@gmail.com>
2020-01-11 13:20:41 -0600

    set up axios POSTing to Firebase

--- src/containers/CoffeePosts/CoffeePosts.js
+++ src/containers/CoffeePosts/CoffeePosts.js
@@ -9,18 +9,38 @@ class CoffeePosts extends Component {
   };
 
   componentDidMount() {
+    this.getPosts();
+  }
+
+  getPosts = () => {
     axios
       .get("https://coffee-locker.firebaseio.com/posts.json")
+      .then(response => this.setState({ posts: response.data }));
+  };
+
+  addPostHandler = () => {
+    const post = {
+      roaster: "Brant",
+      origin: "Columbian",
+      region: "Los Atmos",
+      process: "Natural",
+      notes: ["blueberry", "honey", "floral"],
+      rating: 9,
+      key: Date.now()
+    };
+
+    axios
+      .post("https://coffee-locker.firebaseio.com/posts.json", post)
       .then(response => {
-        this.setState({ posts: response.data });
+        this.getPosts();
       });
-  }
+  };
 
   render() {
     let coffeePosts = <p>Loading...</p>;
     if (this.state.posts) {
-      console.log(this.state.posts);
-      coffeePosts = Object.keys(this.state.posts).map(post => {
+      const reversedCoffeePosts = Object.keys(this.state.posts).reverse()
+      coffeePosts = reversedCoffeePosts.map(post => {
         return (
           <CoffeePost
             roaster={this.state.posts[post].roaster}
@@ -29,13 +49,18 @@ class CoffeePosts extends Component {
             process={this.state.posts[post].process}
             notes={this.state.posts[post].notes}
             rating={this.state.posts[post].rating}
-            key={post}
+            key={this.state.key}
           />
         );
       });
     }
 
-    return <div>{coffeePosts}</div>;
+    return (
+      <div>
+        <button onClick={this.addPostHandler}>Add Post</button>
+        <div>{coffeePosts}</div>;
+      </div>
+    );
   }
 }
 
