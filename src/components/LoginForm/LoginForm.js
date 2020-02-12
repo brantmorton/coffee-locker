16b0c7340377a7d27161534051933671c7df6fac
brantmorton <brantmort@gmail.com>
2020-02-11 21:28:48 -0600

    spiced up the home page

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -1,13 +1,19 @@
 import React, { Component } from "react";
 
 import Button from "../../components/UI/Button/Button";
+import styles from "./LoginForm.module.css";
 
 class LoginForm extends Component {
   render() {
     return (
       <div>
-        <p>Welcome to coffee locker!</p>
-        <p>Please sign in or register</p>
+        <p className={styles.welcomeText}>nice to have you</p>
+        <img
+          className={styles.loginImage}
+          src="login.svg"
+          alt="person on laptop"
+        />
+        <p className={styles.welcomeText}>please sign in or register below</p>
         <Button clicked={this.props.auth.login}>Log In</Button>
       </div>
     );
